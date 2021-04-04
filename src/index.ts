68ca1519dd557ff10ecce27ce2e5164f0ede8a89
Matteo Pieroni <matteopieroni6@gmail.com>
2021-04-04 20:48:39 +0100

    Switched to access token directly

--- src/index.ts
+++ src/index.ts
@@ -1,21 +1,19 @@
 import { changeImage } from "./change-image.js";
 
-const changePic = async (token: Combat.Combatant, isCombat?: boolean) => {
+const changePic = async (combatant: Combat.Combatant, isCombat?: boolean) => {
 	if (!canvas.ready) {
 		return;
 	}
 
-	const tokens = canvas.tokens.placeables.filter(tok => tok.id === token.tokenId);
+	const token = canvas.tokens.get(combatant.tokenId);
 
-	if (tokens.length === 0) {
+	if (!token) {
 		console.log('No token was found')
 		return;
 	}
 
-	const allChanges = tokens.map(tok => changeImage(tok, isCombat));
-
-	await Promise.all(allChanges);
+	await changeImage(token, isCombat);
 }
 
-Hooks.on('createCombatant', (_, token: Combat.Combatant) => changePic(token, true));
-Hooks.on('deleteCombatant', (_, token: Combat.Combatant) => changePic(token, false));
+Hooks.on('createCombatant', (_, combatant: Combat.Combatant) => changePic(combatant, true));
+Hooks.on('deleteCombatant', (_, combatant: Combat.Combatant) => changePic(combatant, false));
